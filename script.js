3791c39d4f32e1e5e6c66fdcc67e45b54e48c726
Oleh <olehpopkov@MacBook-Pro-Oleh.local>
2024-05-02 01:03:40 +0300

    validation, price in days

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
 var phoneInput = document.querySelector('.phone')
 phoneInput.addEventListener('keydown', function(event) {
-
    if( !(event.key == 'ArrowLeft' ||  event.key == 'ArrowRight' ||  event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
     var mask = '+380 (11) 111-11-11'; 
  
@@ -29,82 +27,66 @@ flatpickr( "#count_price",{
     minDate: "today",
     dateFormat: "Y-m-d",
     disable: [
-
-
         {
         from:"2024-04-11",
         to:"2024-04-13"
         }
-    ]
-     
+    ]    
 });
 
 function countPrice() {
-
     var obj = document.getElementById("count_price").value.toString();;
-    if (obj.length > 10){
-        const price = 3000;
-        var price_out = document.getElementById("price_out");
-        price_out.style.display = "block";
-        let dates = obj.split(" to ");
-    
-        let data1 = new Date(dates[0]);
-        let data2 = new Date(dates[1]);
-      
+    const price = 3000;
+    var price_out = document.getElementById("price_out");
+    price_out.innerHTML = (1+" день");
+    price_out2.innerHTML = (price+" грн.");
+    price_out.style.display = "block";
+    let dates = obj.split(" to ");
+    let data1 = new Date(dates[0]);
+    let data2 = new Date(dates[1]); 
+    let obj2 = (Math.round((data2.getTime() - data1.getTime()) / (1000 * 3600 * 24)) + 1);
 
-        let obj2 = Math.round((data2.getTime() - data1.getTime()) / (1000 * 3600 * 24));
-        
-        price_out.innerHTML = (obj2+"ночі");
-        price_out2.innerHTML = (price*obj2);
+    if (obj2 == "NaN"){
+        price_out.innerHTML = (1+" день");
+        price_out2.innerHTML = (price+" грн.");
     }
-    else {
-        return; 
+    else if (obj2 < 5) {
+        price_out.innerHTML = (obj2+" дні");
+        price_out2.innerHTML = (price*obj2+" грн.");
     }
-
+    else if (obj2 > 5) {
+        price_out.innerHTML = (obj2+" днів");
+        price_out2.innerHTML = (price*obj2+" грн.");
+    } 
 }
-
-function validateForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    if (name === "" || email === "") {
-      alert("Пожалуйста, заполните все поля формы.");
-      return false;
-    } else {
-      // Если все поля заполнены, можно выполнить другие действия, например, отправить данные на сервер
-      // Например, можно использовать AJAX для отправки данных без перезагрузки страницы
-      alert("Все поля формы заполнены!");
-      return true;
-    }
-    }
-
 let popupBg = document.querySelector('.popup_bg');
 let popup = document.querySelector('.popup');
 let openPopupButtons = document.querySelectorAll('.open-popup');
 let closePopupButton = document.querySelector('.close-popup');
 
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popupBg.classList.add('active');
-        popup.classList.add('active');
-    })
-});
 function popup_activate (){
     popupBg.classList.add('active');
     popup.classList.add('active');
-
 }
-closePopupButton.addEventListener('click',() => {
+function popup_deactivate (){
     popupBg.classList.remove('active');
     popup.classList.remove('active');
-});
 
-document.addEventListener('click', (e) => {
-    if(e.target === popupBg) {
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
+}
+function validateForm() {
+    var fullname = document.getElementById("fullname").value;
+    var surname = document.getElementById("surname").value;
+    var phone = document.getElementById("phone").value;
+    var count_price = document.getElementById("count_price").value;   
+    if (fullname === "" || surname === "" || phone === "" || count_price === "") {
+      console.log("Пожалуйста, заполните все поля формы.");
+      return false;
+    } else {
+        popup_activate()
+        console.log("wfwef");
+      return true;
     }
-});
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     const peopleCount = document.getElementById("peopleCount");
